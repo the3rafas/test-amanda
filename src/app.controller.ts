300f271ec80649e22351e27ba93f9da90844340c
the3rafas <the_3rafas@baianat.com>
2023-07-27 11:54:56 +0300

    initialize

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Request } from 'express';
+
+@Controller('amanda')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/test')
+  testGet(@Req() req: Request): string {
+    console.log('Get test ****************************************');
+    console.log('req.params ***************');
+    console.log(req.params);
+    console.log('req.body ***************');
+    console.log(req.body);
+    return 'hi';
+  }
+  @Post('/test')
+  testPost(@Req() req: Request): string {
+    console.log('Post test ****************************************');
+    console.log('req.params ***************');
+    console.log(req.params);
+    console.log('req.body ***************');
+    console.log(req.body);
+
+    return 'hi';
+  }
+}
